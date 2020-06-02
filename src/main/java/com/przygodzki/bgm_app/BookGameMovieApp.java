4a2b0c203ba17384e8b239afce44218fe47f34d3
ChristianPrzygodzki <chrisorian@o2.pl>
2020-06-02 12:34:26 +0200

    Test commit
    Added comment to App file to check what can i push.

--- src/main/java/com/przygodzki/bgm_app/BookGameMovieApp.java
+++ src/main/java/com/przygodzki/bgm_app/BookGameMovieApp.java
@@ -1,12 +1,30 @@
 package com.przygodzki.bgm_app;
 
+import com.przygodzki.bgm_app.entity.Game;
+import com.przygodzki.bgm_app.repository.GameRepository;
+import com.przygodzki.bgm_app.service.GameService;
+import com.przygodzki.bgm_app.service.implementation.GameServiceImpl;
+import com.przygodzki.bgm_app.to.GameTo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
 
 @SpringBootApplication
 public class BookGameMovieApp {
 
+
+    private static GameRepository gameRepo;
+
+    @Autowired
+    public void setGameRepo(GameRepository gameRepo) {
+        BookGameMovieApp.gameRepo = gameRepo;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(BookGameMovieApp.class, args);
+        /*List<Game> lista = gameRepo.findAll();
+        System.out.println(lista.size());*/
     }
 }
